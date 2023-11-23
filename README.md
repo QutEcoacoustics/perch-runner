29cd5ff4301b8d92e30cce18e294416e1f5646bc
peichins <peichins@users.noreply.github.com>
2023-11-23 16:55:50 +1100

    finished working version of embed script outputting to csv

--- README.md
+++ README.md
@@ -1,2 +1,134 @@
-# A docker container to train linear models from chirp embeddings and to run inference using this model on embeddings of long files
+# chirp-runner
 
+A docker container that runs chirp.
+
+## Commands
+
+This container has three basic functions:
+
+1. Generate chirp embeddings for a given audio file
+2. To classify the embeddings given an audio file and one or more additional linear classifiers
+3. To measure distance between a given sample and a given audio file
+
+All three commands output results in 5 second chunks.
+
+At this time we're choosing not to reuse embeddings. It is easier to (though much much slower)
+to ignore the caching and model version problems that come with reusing embeddings.
+
+### Generate embeddings
+
+The `generate` command accepts one input file and one output directory.
+
+- One file will be produced containing the embeddings for the input file.
+- The file MUST be in a self-describing format.
+- Each result in the file must have at least
+  - a `start` value representing the start of the record in seconds
+  - an `embedding` value representing the embedding for the record
+    - For CSV this should be a Base64 encoded string of the embedding vector
+  - a `source_separation_channel` representing the virtual channel the record came from
+  - and other columns are allowed as needed
+- Files should include provenance metadata in their headers
+- Note: audio recordings IDs (or other source identifying information) are
+  not needed. The assumption is that this container is run by an orchestrator
+  that knows what audio file is being processed.
+
+#### Usage
+
+```
+generate [-F|--format] [-c|--config <config-file>] <input-file> <output-directory>
+    <input-file>                The file to generate embeddings for
+    <output-directory>          The directory to write the embeddings to
+    -F|--format [csv|parquet]   The format of the embeddings file: `CSV`` or `Parquet``
+    -c|--config <config-file>   An optional configuration file to use
+```
+
+#### Configuration file
+
+```yaml
+hop_length_seconds: 5.0
+```
+
+#### Example
+
+```bash
+cd /data
+curl -o audio.wav https://api.ecosounds.org/audio_recordings/123.wav
+docker run -v /data:/data chirp-runner generate /data/audio.wav /data/output
+```
+
+### Classify
+
+The `classify` command accepts one input file, one output directory, and one or more classifiers.
+The classifiers are provided in a config file.
+
+- One file will be produced containing the classifications for the input file.
+- The file MUST be in a self-describing format.
+- The file should return results in \<insert link to our event common format\>
+- Files should include provenance metadata in their headers
+
+#### Usage
+
+```
+classify [-F|--format] [-c|--config <config-file> ] <input-file> <output-directory>
+    <input-file>                The file to generate embeddings for
+    <output-directory>          The directory to write the embeddings to
+    -c|--config <config-file>   An optional configuration file to use
+```
+
+#### Configuration file
+
+```yaml
+hop_length_seconds: 5.0
+classifiers:
+  - name: "classifier1"
+    tags: ["tag1", "tag2"]
+    # base64 encoded model
+    model: "asd321jhfsdhk438asdmlfas89i3mnlksf..."
+  - name: "classifier2"
+    tags: ["tag3", "tag4"]
+     # base64 encoded model
+    model: "..."
+
+```
+
+#### Example
+
+```bash 
+cd /data
+curl -o audio.wav https://api.ecosounds.org/audio_recordings/123.wav
+docker run -v /data:/data chirp-runner classify -c /data/config.yml /data/audio.wav /data/output
+```
+
+### Distance
+
+The `distance` command accepts one input file, one output directory, and a query file.
+
+- The query file must be an audio file that is 5 seconds long.
+- The output of this command is the same as the output of the `generate` command,
+    but with an additional column `distance` that represents the distance between the
+    query and the sample.
+
+#### Usage
+
+```
+distance [-F|--format] [-c|--config <config-file> ] <input-file> <query-file> <output-directory> 
+    <input-file>                The file to generate embeddings for
+    <output-directory>          The directory to write the embeddings to
+    <query-file>                The file to use as the query
+    -c|--config <config-file>   An optional configuration file to use
+```
+
+#### Configuration file
+
+```yaml
+hop_length_seconds: 5.0
+```
+
+#### Example
+
+```bash
+cd /data
+curl -o audio.wav https://api.ecosounds.org/audio_recordings/123.wav
+curl -o query.wav https://api.ecosounds.org/audio_recordings/456/media.wav?start=10&end=15
+docker run -v /data:/data chirp-runner distance -c /data/config.yml /data/audio.wav /data/query.wav /data/output
+```
--- embed_audio_slim.py
+++ embed_audio_slim.py
@@ -2,200 +2,87 @@
 # we don't need to store information about the file that the example comes from
 
 # Global imports
-from etils import epath
+from pathlib import Path
 import numpy as np
-import tensorflow as tf
-import tqdm
-# from chirp.inference import colab_utils
-# colab_utils.initialize(use_tf_gpu=True, disable_warnings=True)
+# import tqdm
 import argparse
+import pandas as pd
+import soundfile
+from math import ceil
+
 
 from chirp import audio_utils
-from chirp import config_utils
-from chirp.configs import config_globals
-from chirp.inference import embed_lib
-from chirp.inference import tf_examples
+
+# we need to house config in this object because
+# it's what TaxonomyModelTF expects. Specifically it needs an object that has dot 
+# access to values as well as dict-like access for using the spread operator (**config) 
+from ml_collections import config_dict
 
 from chirp.inference.models import TaxonomyModelTF
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--source_file")
-parser.add_argument("--output_dir")
-
+parser.add_argument("--source_file", help="path to the file to analyze")
+parser.add_argument("--output_folder", help="file to embeddings to")
+parser.add_argument("--max_segments", default=-1, type=int, help="only analyse this many segments of the file. Useful for debugging quickly. If ommitted will analyse all")
+parser.add_argument("--segment_length", default=60, type=int,  help="the file is split into segments of this duration in sections to save loading entire files into ram")
+parser.add_argument("--hop_size", default=5, type=float,  help="create an 5 second embedding every this many seconds. Leave as default 5s for no overlap and no gaps.")
 
 args = parser.parse_args()
 
-
-# Name of base configuration file in `chirp/inference/configs`
-config_key = 'raw_soundscapes'  #@param
-config = embed_lib.get_config(config_key)
-config = config_utils.parse_config(config, config_globals.get_globals())
-
-# Here we adjust the input and output targets.
-# config.output_dir = 'output/all_02'  #@param
-# config.source_file_patterns = ['/napco_survey_project_audio/PW/inference_datasets/20230413/*/*/*.wav']  #@param
-
-config.output_dir = args.output_dir  #@param
-# config.source_file_patterns = [args.source_file_patterns]  #@param
+# check audio exists and get the duration
+audio_file = soundfile.SoundFile(args.source_file)
+audio_duration = audio_file.frames / audio_file.samplerate
+print(f'analysing {args.source_file} samples: {audio_file.frames}, sr: {audio_file.samplerate}, duration {audio_duration} sec')
 
 
+# TODO: build into image
+model_path = "/phil/bird-vocalization-classifier_3/"
 
-# Define the model
-model_choice = 'perch'
-# For Perch, the directory containing the model.
-# For BirdNET, point to the specific tflite file.
-model_path = "/phil/bird-vocalization-classifier_3/"  #@param
-config.embed_fn_config.model_config.model_path = model_path
+model_config = config_dict.create(
+    hop_size_s = args.hop_size,
+    model_path = model_path,
+    sample_rate = 32000,
+    window_size_s = 5.0
+)
 
-config.embed_fn_config.model_config.window_size_s = 5.0
-config.embed_fn_config.model_config.hop_size_s = 5.0
-config.embed_fn_config.model_config.sample_rate = 32000
-
-
-# Only write embeddings to reduce size.
-config.embed_fn_config.write_embeddings = True
-config.embed_fn_config.write_logits = False
-config.embed_fn_config.write_separated_audio = False
-config.embed_fn_config.write_raw_audio = False
+output_folder = Path(args.output_folder)
+output_folder.mkdir(exist_ok=True, parents=True)
 
 
-# Embedding windows are broken up into groups, typically one minute in length.
-# This lets us limit input size to the model, track progres and
-# recover from failures more easily.
-
-# the amount of audio to load at a time
-config.shard_len_s = 60  #@param
-
-# I think we can ignore this
-# config.num_shards_per_file = 1  #@param
-
-# Number of parent directories to include in the filename.
-config.embed_fn_config.file_id_depth = 2
-
-# Number of TF Record files to create.
-# I think we should be able to ignore this too. 
-# we want 
-config.tf_record_shards = 1  #@param
-
-# Speech filter threshold for YamNet.
-# Set to a value between 0 and 1, or -1 to disable.
-# seems we don't need this anymore
-# config.embed_fn_config.speech_filter_threshold = -1.0
-
-
-
-# Create output directory and write the configuration.
-output_dir = epath.Path(config.output_dir)
-output_dir.mkdir(exist_ok=True, parents=True)
-embed_lib.maybe_write_config(config, output_dir)
-
-# # Create SourceInfos.
-# source_infos = embed_lib.create_source_infos(
-#     config.source_file_patterns,
-#     config.num_shards_per_file,
-#     config.shard_len_s)
-# print(f'Found {len(source_infos)} source infos.')
-
-# Set up the embedding function, including loading models.
-# embed_fn = embed_lib.EmbedFn(**config.embed_fn_config)
 print('\n\nLoading model(s)...')
-# embed_fn.setup()
+#embedding_model = TaxonomyModelTF.from_config(config.embed_fn_config["model_config"])
+embedding_model = TaxonomyModelTF.from_config(model_config)
 
-embedding_model = TaxonomyModelTF.from_config(config.embed_fn_config["model_config"])
+# an empty array of the with zero rows to concatenate to
+# 1280 embeddings plus one column for the offset_seconds
+file_embeddings = np.empty((0,1,1281))
 
+total_segments = ceil(audio_duration / args.segment_length)
+num_segments = min(args.max_segments, total_segments) if args.max_segments > 1 else total_segments
 
-# we now need to call embed on the embedding model, passing 60s chunks as np arrays
-# 1. use soundfile to load a segment of audio
-max_segments = 15
-segment_length = 60
-for segment_num in range(0..max_segments):
-  offset_s = segment_length * segment_num
+for segment_num in range(num_segments):
+  offset_s = args.segment_length * segment_num
+  print(f'getting embeddings for offsets {offset_s} to {offset_s + args.segment_length}')
 
   audio = audio_utils.load_audio_window(
       args.source_file, offset_s, 
-      config.embed_fn_config.model_config.sample_rate, segment_length
+      model_config.sample_rate, args.segment_length
   )
 
   if len(audio) > 1:
-    segment_embedding = embedding_model.embed(audio)
+    segment_embeddings = embedding_model.embed(audio)
+    offsets = np.arange(0,60,5).reshape(12,1,1)
+    segment_embeddings = np.concatenate((offsets + offset_s, segment_embeddings.embeddings), axis=2)
+    file_embeddings = np.concatenate((file_embeddings, segment_embeddings), axis=0)
+  else:
+    print('no audio found')
+  
 
+for channel in range(file_embeddings.shape[1]):
+    channel_embeddings = file_embeddings[:,channel,:]
+    df = pd.DataFrame(channel_embeddings, columns=['offset'] + ['e' + str(i).zfill(3) for i in range(1280)])
 
+    destination_filename = f"embeddings_{channel}.csv"
 
-print('\n\nTest-run of model...')
-# We run the test twice - the first run optimizes the execution, and
-# subsequent runs will be full-speed.
-window_size_s = config.embed_fn_config.model_config.window_size_s
-sr = config.embed_fn_config.model_config.sample_rate
-z = np.zeros([int(sr * window_size_s)])
-embed_fn.embedding_model.embed(z)
-print('Setup complete!')
-
-#@title Run embedding. (safe) { vertical-output: true }
-
-# Loads audio files one-by-one using methods which will tend not to fail
-# if the target files have minor problems (eg, wrong length metadata).
-
-embed_fn.min_audio_s = 1.0
-record_file = (output_dir / 'embeddings.tfrecord').as_posix()
-succ, fail = 0, 0
-with tf_examples.EmbeddingsTFRecordMultiWriter(
-    output_dir=output_dir, num_files=config.tf_record_shards) as file_writer:
-  for source_info in tqdm.tqdm(source_infos):
-    examples = embed_fn.process(source_info=source_info)
-    if examples is None:
-      fail += 1
-      continue
-    for example in examples:
-      file_writer.write(example.SerializeToString())
-    succ += 1
-  file_writer.flush()
-print(f'\n\nSuccessfully processed {succ} source_infos, failed {fail} times.')
-
-fns = [fn for fn in output_dir.glob('embeddings-*')]
-ds = tf.data.TFRecordDataset(fns)
-parser = tf_examples.get_example_parser()
-ds = ds.map(parser)
-for ex in ds.as_numpy_iterator():
-  print(ex['filename'])
-  print(ex['embedding'].shape)
-  break
-
-#@title Run embedding. (fast) { vertical-output: true }
-
-# Uses multiple threads to load audio before embedding.
-# This tends to be faster, but can fail if any audio files are corrupt.
-
-embed_fn.min_audio_s = 1.0
-record_file = (output_dir / 'embeddings.tfrecord').as_posix()
-succ, fail = 0, 0
-
-audio_iterator = audio_utils.multi_load_audio_window(
-    filepaths=[s.filepath for s in source_infos],
-    offsets=[s.shard_num * s.shard_len_s for s in source_infos],
-    sample_rate=config.embed_fn_config.model_config.sample_rate,
-    window_size_s=config.shard_len_s,
-)
-with tf_examples.EmbeddingsTFRecordMultiWriter(
-    output_dir=output_dir, num_files=config.tf_record_shards) as file_writer:
-  for source_info, audio in tqdm.tqdm(
-      zip(source_infos, audio_iterator), total=len(source_infos)):
-    file_id = source_info.file_id(config.embed_fn_config.file_id_depth)
-    offset_s = source_info.shard_num * source_info.shard_len_s
-    example = embed_fn.audio_to_example(file_id, offset_s, audio)
-    if example is None:
-      fail += 1
-      continue
-    file_writer.write(example.SerializeToString())
-    succ += 1
-  file_writer.flush()
-print(f'\n\nSuccessfully processed {succ} source_infos, failed {fail} times.')
-
-fns = [fn for fn in output_dir.glob('embeddings-*')]
-ds = tf.data.TFRecordDataset(fns)
-parser = tf_examples.get_example_parser()
-ds = ds.map(parser)
-for ex in ds.as_numpy_iterator():
-  print(ex['filename'])
-  print(ex['embedding'].shape)
-  break
+    df.to_csv(destination_filename, index=False)
