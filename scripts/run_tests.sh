678abcebf798aad700ec7849da9e2290b4eba3df
peichins <peichins@users.noreply.github.com>
2024-03-18 20:11:30 +1000

    bug in action

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -4,8 +4,10 @@
 # To run tests in the container, we mount the test directory
 
 # run tests from within the container
+set -x
 docker run \
 -v $(pwd)/tests/:/app/tests \
 --entrypoint python \
 qutecoacoustics/perchrunner:latest -m pytest /app/tests/app_tests
+set +x
 
